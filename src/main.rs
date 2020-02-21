94a3ae56cc1645cc0e17afb71cca4d76afc3ac58
Mark Jordan <mark.jordan@red-gate.com>
2020-02-21 15:53:56 +0000

    start figuring out tui/crossterm code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::terminal::enable_raw_mode;
+use std::io::stdout;
+use tui::backend::CrosstermBackend;
+use tui::Terminal;
+
+fn main() -> Result<(), failure::Error> {
+    enable_raw_mode()?;
+    let mut stdout = stdout();
+
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    Ok(())
 }
