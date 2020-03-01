66257444ed410a19fceaae8d61f86d1b4f0705cd
Nyctef <nyctef@nyctef.com>
2020-03-01 15:05:11 +0000

    starting to parse response

--- src/teamcity.rs
+++ src/teamcity.rs
@@ -1,3 +1,19 @@
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+enum BuildStatus {
+    SUCCESS,
+    FAILURE,
+    ERROR,
+}
+
+#[derive(Debug, Deserialize)]
+struct BuildJson {
+    id: i32,
+    status: BuildStatus,
+    composite: bool,
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -9,7 +25,17 @@ mod tests {
     async fn can_poke_tc_api() -> Fallible<()> {
         let tc_token = env::var("TCUI_TC_TOKEN").expect("TCUI_TC_TOKEN is required");
         let url = format!("{tcRoot}/app/rest/builds/buildType:{buildType},defaultFilter:false,branch:name:{branchName}", tcRoot="https://buildserver.red-gate.com", buildType="RedgateChangeControl_OverallBuild", branchName="add-beta-tag");
-        let latest_build = reqwest::Client::new().get(&url).send().await?;
+        let latest_build: BuildJson = reqwest::Client::new()
+            .get(&url)
+            .header(
+                "Authorization",
+                format!("Bearer {tc_token}", tc_token = tc_token),
+            )
+            .header("Accept", "application/json")
+            .send()
+            .await?
+            .json()
+            .await?;
         println!("{:#?}", latest_build);
         Ok(())
     }
