0f6e16a246e2e28ab0964f96cd5350f6858a4b32
Mark Jordan <mark.jordan@red-gate.com>
2020-03-06 16:41:11 +0000

    expand teamcity api deserialization to cover basic stuff we need

--- src/teamcity.rs
+++ src/teamcity.rs
@@ -8,10 +8,37 @@ enum BuildStatus {
 }
 
 #[derive(Debug, Deserialize)]
+#[serde(rename_all = "lowercase")]
+enum BuildState {
+    Queued,
+    Running,
+    Finished,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
 struct BuildJson {
-    id: i32,
+    number: String,
     status: BuildStatus,
-    composite: bool,
+    state: BuildState,
+    web_url: String,
+    #[serde(rename = "snapshot-dependencies")]
+    snapshot_dependencies: SnapshotDependencies
+}
+
+#[derive(Debug, Deserialize)]
+struct SnapshotDependencies {
+    build: Vec<BuildDependency>
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct BuildDependency {
+    number: String,
+    status: BuildStatus,
+    state: BuildState,
+    percentage_complete: Option<i32>,
+    web_url: String,
 }
 
 #[cfg(test)]
@@ -24,6 +51,7 @@ mod tests {
         let tc_token = env::var("TCUI_TC_TOKEN").expect("TCUI_TC_TOKEN is required");
         let url = format!("{tcRoot}/app/rest/builds/buildType:{buildType},defaultFilter:false,branch:name:{branchName}", tcRoot="https://buildserver.red-gate.com", buildType="RedgateChangeControl_OverallBuild", branchName="add-beta-tag");
         let response = ureq::get(&url)
+            .query("fields", "number,status,state,percentageComplete,webUrl,snapshot-dependencies(build(number,status,state,percentageComplete,webUrl))")
             .set(
                 "Authorization",
                 &format!("Bearer {tc_token}", tc_token = tc_token),
