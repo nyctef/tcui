781c56f42955f9b173606ff55d69ce1cf070c5d8
Nyctef <nyctef@nyctef.com>
2020-03-08 10:39:07 +0000

    add build name to tc api query

--- src/teamcity.rs
+++ src/teamcity.rs
@@ -17,18 +17,24 @@ enum BuildState {
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct BuildJson {
+struct Build {
     number: String,
     status: BuildStatus,
     state: BuildState,
     web_url: String,
     #[serde(rename = "snapshot-dependencies")]
-    snapshot_dependencies: SnapshotDependencies
+    snapshot_dependencies: SnapshotDependencies,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct BuildType {
+    name: String,
 }
 
 #[derive(Debug, Deserialize)]
 struct SnapshotDependencies {
-    build: Vec<BuildDependency>
+    build: Vec<BuildDependency>,
 }
 
 #[derive(Debug, Deserialize)]
@@ -50,17 +56,20 @@ mod tests {
     fn can_poke_tc_api() {
         let tc_token = env::var("TCUI_TC_TOKEN").expect("TCUI_TC_TOKEN is required");
         let url = format!("{tcRoot}/app/rest/builds/buildType:{buildType},defaultFilter:false,branch:name:{branchName}", tcRoot="https://buildserver.red-gate.com", buildType="RedgateChangeControl_OverallBuild", branchName="add-beta-tag");
+        // println!("Requesting url {}", url);
         let response = ureq::get(&url)
-            .query("fields", "number,status,state,percentageComplete,webUrl,snapshot-dependencies(build(number,status,state,percentageComplete,webUrl))")
+            .query("fields", "number,status,state,percentageComplete,webUrl,buildType(name),snapshot-dependencies(build(webUrl,number,status,state,percentageComplete,buildType(name)))")
             .set(
                 "Authorization",
                 &format!("Bearer {tc_token}", tc_token = tc_token),
             )
             .set("Accept", "application/json")
-            .call()
-            .into_json()
-            .unwrap();
-        let latest_build = serde_json::from_value::<BuildJson>(response).unwrap();
+            .call();
+        // println!("{}", response.status_line());
+        // println!("{:?}", response.into_string());
+        let json = response.into_json().unwrap();
+        // println!("{:#?}", json);
+        let latest_build = serde_json::from_value::<Build>(json).unwrap();
         println!("{:#?}", latest_build);
     }
 }
